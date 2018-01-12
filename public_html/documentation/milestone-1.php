67203bdbaf0e65b730ff26350de03efd34738e8e
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-11 19:22:51 -0700

    added persona information

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -27,11 +27,12 @@
 		<div>
 			<h2>Persona:</h2>
 			<ul>
-				<li>Name: </li>
-				<li>A detailed description that includes demographic information such as: age, job or role, relevant technology being used, behaviors and attitudes, and the situation or scenario in which they will use your site: </li>
-				<li>Goal(s) regarding the use of your site: </li>
-				<li>Frustrations your persona may be facing: </li>
-				<li>Additional information your persona may need in order to achieve their goal: </li>
+				<li>Name: Alexandra Moore</li>
+				<!--A detailed description that includes demographic information such as: age, job or role, relevant technology being used, behaviors and attitudes, and the situation or scenario in which they will use your site:-->
+				<li>Description: Alexandra is the owner of a business, Meetoo Enterprises, that makes apps for direct realease to the public, and also creates apps to-order for clients.  They have several profitable apps already completed, and are continuing to create apps in the popular style of their first. Alexandra is committed to empowering women to break glass ceilings and reach their full potential.  She insists that all products made by Meetoo Enterprises take this into consideration as one of their goals.</li>
+				<li>Goal(s) regarding the use of your site: Alexandra is seeking another full-stack web developer to add to her team, initially as an intern, but moving to an employee once she determines if they are a "good-fit." She is happy to train this new person exactly to her specifications, but needs someone who already has the basics, and preferrably, some artistic skills. She favors hiring people who are well-rounded and have the ability to "see the big picture."  Alexandra is very busy, and is swamped by applications for this opportunity. She feels that when viewing an applicant's website, it should grab her attention and make her take a second look. It should also appear to be a good fit-design-wise for her company.</li>
+				<li>Frustrations your persona may be facing: She has waaaay too many resumes and portfolios to look at and not enough time. She finds it can be hard to truly judge if a person will be a good fit for her team, just by looking at a resume or generic portfolio website.  She has hired well-trained people before, and had found issues after much investment and training, especially that they were not personally responsible enough, or just didn't find the excitement necessary for the job.  She has also had issues with employees not being willing or able to take on extra responsibilities that come up from time to time in a small business with a limited staff, who often have to wear many hats.  She wants an employee who is aware of what is going on in as a whole within the company, so that they can become a true team member, and not get solely focused on their tiny task, without seeing how it relates to everything else, and where it is going.</li>
+				<li>Additional information your persona may need in order to achieve their goal: Alexandra needs to know whether I am going to be a smart worker, and not just a hard worker.  She needs to know that I can definitely see the big picture, am good with systems and at anticipating not just the next step in a project, but potential pitfalls and outcomes from design decisions made without the  benefit of hindsight.  She wants to know that I have foresight, the skills to match my knowledge, and the ability to research and find solutions, even if I don't know the answer to start out.</li>
 			</ul>
 		</div>
 
