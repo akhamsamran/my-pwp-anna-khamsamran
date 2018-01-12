dec21a1a051a6c2e3140840d27d86df9578a142d
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-11 18:59:54 -0700

    added directories public_html > documentation and file: milestone-1.php

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna
+ * Date: 1/11/2018
+ * Time: 6:49 PM
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Anna Khamsamran's Personal Website</title>
+	</head>
+	<body>
+		<h1>Anna Khamsamran's Personal Website Project-Brief Overview</h1>
+		<p>Due Date: Jan 18, 2018</p>
+		<div>
+			<h2>1. Purpose: This website will serve as a portfolio of my work.</h2>
+			<p></p>
+			<h2>2. Audience: Employers.</h2>
+			<p></p>
+			<h2>3. Goal: To get hired at an internship or job.</h2>
+			<p></p>
+		</div>
+		<div>
+			<h2>Persona:</h2>
+			<ul>
+				<li>Name: </li>
+				<li>A detailed description that includes demographic information such as: age, job or role, relevant technology being used, behaviors and attitudes, and the situation or scenario in which they will use your site: </li>
+				<li>Goal(s) regarding the use of your site: </li>
+				<li>Frustrations your persona may be facing: </li>
+				<li>Additional information your persona may need in order to achieve their goal: </li>
+			</ul>
+		</div>
+
+	</body>
+</html>
