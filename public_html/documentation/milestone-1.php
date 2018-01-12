cfb9511d4980cf0ef2014e1fc45057365c3316b3
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-11 19:25:58 -0700

    changed h2 to h3

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -17,15 +17,15 @@
 		<h1>Anna Khamsamran's Personal Website Project-Brief Overview</h1>
 		<p>Due Date: Jan 18, 2018</p>
 		<div>
-			<h2>1. Purpose: </h2>
+			<h3>Purpose: </h3>
 			<p>This website will serve as a portfolio of my work.</p>
-			<h2>2. Audience: </h2>
+			<h3>Audience: </h3>
 			<p>Employers.</p>
-			<h2>3. Goal:</h2>
+			<h3>Goal:</h3>
 			<p>To get hired at an internship or job.</p>
 		</div>
 		<div>
-			<h2>Persona:</h2>
+			<h3>Persona:</h3>
 			<ul>
 				<li>Name: Alexandra Moore</li>
 				<!--A detailed description that includes demographic information such as: age, job or role, relevant technology being used, behaviors and attitudes, and the situation or scenario in which they will use your site:-->
