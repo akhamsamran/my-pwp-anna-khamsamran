a1a7125a9ea83fcd9e6f4d84bb4313d0d6008d64
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-18 15:56:20 -0700

    created milestone-2.php

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna
+ * Date: 1/18/2018
+ * Time: 3:55 PM
+ */?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Anna Khamsamran's Personal Website</title>
+		<link rel="stylesheet" href="css/style.css"/>
+	</head>
+	<body>
+		<h1></h1>
+
+	</body>
+</html>
