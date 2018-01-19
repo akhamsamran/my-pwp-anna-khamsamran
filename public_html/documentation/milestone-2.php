a35249630666178ff8fe7f91d8f9fd38238e6e07
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-18 21:08:06 -0700

    re-ordered sections

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -15,20 +15,22 @@
 	<body>
 		<h1>PWP Milestone 2&alpha;</h1>
 		<h2>Content Strategy</h2>
-		<p><em>My one-page site will include these sections:</em></p>
+		<p><em>My one-page site will be a statement piece of svg graphics. A complete svg art piece will serve as both the backgound, and have harmoniously integrated sections for the following(alternatively the svg can be broken down to fit within the framework of Bootstrap to appear as though is a complete art piece):</em></p>
 		<div>
 			<h3>Identity/Welcome:</h3>
 			<p>A header section including a personal logo with my name. This will include statement or title which indicates my skillset.</p>
+			<h3>Links/Contact:</h3>
+			<a>This section will consist of links to my professional social media profiles (GitHub, LinkedIn, Twitter), my professional email, and my online store (Cafepress).</a>
 			<h3>Resume/About Me:</h3>
 			<ol>
-				<li>some information about me personally.</li>
-				<li>Highlights of my resume(The full resume will be accessible as a link to a pdf document).</li>
+				<li>some information about me personally. This section will be short, but friendly and impersonal to add interest and to indicate that I am approachable.</li>
+				<h3>Portfolio:</h3>
+				<a>An animated portfolio of examples of my work (images and artistic renderings of websites or databases with >=5 items. Each item should include a short description.</a>
+				<li>Highlights of my resume as relate to art and technology. Technology will be represented by icons of the representative programming languages and programs. (The full resume will be accessible as a link to a pdf document).</li>
 			</ol>
-
 			<h3>Portfolio:</h3>
-			<a></a>
-			<h3>Links/Contact:</h3>
-			<a></a>
+			<a>An animated portfolio of examples of my work (images and artistic renderings of websites or databases with >=5 items. Each item should include a short description.</a>
+
 
 
 		</div>
