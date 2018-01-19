61160a62749835ad83d3f0bdba63b06ecfbcffa9
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-18 18:47:34 -0700

    added to information about me for the plan for the webpage

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -13,10 +13,22 @@
 		<link rel="stylesheet" href="css/style.css"/>
 	</head>
 	<body>
-		<h1>PWP Milestone &alpha;</h1>
+		<h1>PWP Milestone 2&alpha;</h1>
 		<h2>Content Strategy</h2>
 		<p><em>My one-page site will include these sections:</em></p>
 		<div>
+			<h3>Identity/Welcome:</h3>
+			<p>A header section including a personal logo with my name. This will include statement or title which indicates my skillset.</p>
+			<h3>Resume/About Me:</h3>
+			<ol>
+				<li>some information about me personally.</li>
+				<li>Highlights of my resume(The full resume will be accessible as a link to a pdf document).</li>
+			</ol>
+
+			<h3>Portfolio:</h3>
+			<a></a>
+			<h3>Links/Contact:</h3>
+			<a></a>
 
 
 		</div>
