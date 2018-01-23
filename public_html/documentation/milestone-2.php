9a90bddafab8cf34cb45ad431ab449bd3783b4ae
Anna Khamsamran <29902095+akhamsamran@users.noreply.github.com>
2018-01-22 21:20:04 -0700

    changed image links

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -35,9 +35,9 @@
 		<div>
 			<h2>Wireframes:</h2>
 			<h3>Mobile Design:</h3>
-			<img src="images/wireframe-phone.svg" alt="wireframe design for mobile"/>
+			<img src="images/wireframe-phone2.svg" alt="wireframe design for mobile"/>
 			<h3>Desktop:</h3>
-			<img src="images/wireframe-desktop.svg" alt="wireframe design for desktop computer"/>
+			<img src="images/wireframe2.svg" alt="wireframe design for desktop computer"/>
 		</div>
 
 
