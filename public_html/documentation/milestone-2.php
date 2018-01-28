5189d99157796633c05a102e9888ab586237f5e9
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-28 12:14:52 -0700

    updated new wireframes

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -29,12 +29,12 @@
 		<div>
 			<h2>Wireframes:</h2>
 			<h3>Mobile Design:</h3>
-			<img src="images/wireframe-phone2.svg" alt="wireframe design for mobile phone"/>
+			<img src="images/wireframePhoneScreen.svg.svg" alt="wireframe design for mobile phone"/>
 			<h3>Desktop:</h3>
-			<img src="images/wireframe2.svg" alt="wireframe design for desktop computer"/>
+			<img src="images/wireframeComputerScreen.svg.svg" alt="wireframe design for desktop computer"/>
 		</div>
 		<div>
-			<h3>Ideas:</h3>
+			<h3>This is a color Mockup, but it has not yet been changed to match new wireframes(just for color ideas)</h3>
 			<img src="images/personalwebsiteideas2.png" alt="a mock-up of an idea for a website"/>
 		</div>
 
