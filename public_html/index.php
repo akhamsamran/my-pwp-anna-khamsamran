34673c73f8670e303a3ff043a20378d2853f9814
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-02-08 12:01:50 -0700

    setting up index.php for bootstrap and basic layout

---
+++ public_html/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+		<!--BOOTSTRAP CSS-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+		<!--Other and custom CSS-->
+		<link rel="stylesheet" href="css/style.css"/>
+
+		<!--Optional Javascript links-->
+		<!--link to Bootstrap javascript dependencies-->
+		<!--(Put it here, but if it is a heavy site, put this block just below the closing body tag)-->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+		<title>Anna Khamsamran</title>
+
+	</head>
+	<body>
+		<header>
+			<div class="container-flex">
+
+
+			</div>
+
+		</header>
+		<main>
+			<div class="container-flex">
+
+
+			</div>
+
+		</main>
+		<footer>
+			<div class="container-flex">
+
+
+			</div>
+
+		</footer>
+
+
+
+
+	</body>
+</html>
