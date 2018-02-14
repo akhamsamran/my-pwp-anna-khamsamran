dc8307e1b970e77b0d67bfb1218332b60c35cb3b
akhamsamran <akamsamran1@cnm.edu>
2018-02-14 16:26:34 -0700

    changed image

--- public_html/index.php
+++ public_html/index.php
@@ -29,7 +29,7 @@
 				<div class="container">
 					<div class="row">
 						<div id="top" class="col">
-							<img src="images/aklogosm.png" alt="logo" width="45" height="30" class="d-inline-block align-top" alt="Anna Khamsamran logo">
+							<img src="images/annakhamsamran-blue-blue.svg" alt="logo" width="45" height="30" class="d-inline-block align-top" alt="Anna Khamsamran logo">
 							Anna Khamsamran Data, Design, Development
 						</div>
 					</div>
