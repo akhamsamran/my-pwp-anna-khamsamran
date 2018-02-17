b203d610618f41cdf52c8c7cdb21c33488f97a48
akhamsamran <akamsamran1@cnm.edu>
2018-02-17 09:59:27 -0700

    added divs for carousel and slider

--- public_html/index.php
+++ public_html/index.php
@@ -22,7 +22,12 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+		<!--Slick Carousel js-->
+		<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<script type="text/javascript" src="slick/slick.min.js"></script>
+		<!--link to initialization script for slick carousel-->
+		<script type="text/javascript" src="js/portfolio.js"></script>
 		<title>Anna Khamsamran</title>
 
 	</head>
@@ -68,8 +73,11 @@
 			</div>
 			<!--the div for portfolio, should align center in all sizes-->
 			<div class="container text-center">
-				<div class="carousel">
+				<div class="slider-for-carousel">
 					<div>your content</div>
+					<div>your content</div>
+					<div>your content</div>
+					<div class="slider-nav-carousel"
 					<div>your content</div>
 					<div>your content</div>
 				</div>
---
+++ public_html/js/portfolio.js
@@ -0,0 +1,15 @@
+$('.slider-for-carousel').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	fade: true,
+	asNavFor: '.slider-nav'
+});
+$('.slider-nav-carousel').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	asNavFor: '.slider-for',
+	dots: true,
+	centerMode: true,
+	focusOnSelect: true
+});
