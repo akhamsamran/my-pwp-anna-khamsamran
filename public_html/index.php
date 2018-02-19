416cf4b86965dc1cb1efdf27a76b8885e6746eec
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-02-19 16:58:45 -0700

    put bio in 3 columns

--- public_html/index.php
+++ public_html/index.php
@@ -133,28 +133,41 @@
 			</div>
 
 			<!--the div for About Me-->
-			<div class="container">
-				<p>About me:</p>
-				<p>At eighteen, I set off towards Nepal with the intention of volunteering in a women’s shelter for a
-				few months before going to college. On the way there, I stopped in Thailand, went to an island
-				and took a scuba diving course, got caught in a typhoon, and missed my flight to Nepal. I never
-				went on to Nepal, never went back to college, and ended up living in Thailand for the next eighteen
-				years. During that time, I found I could pursue my interests for the sheer joy of learning and
-				adventure. I realized that my hard work and perseverance could bring me success. I pushed through
-				the self-doubt that comes from following the unbeaten path, and managed to teach myself to
-				speak Thai, start several businesses, provide services to clients from all over the world, and learned
-				how to flourish in a different culture. When my daughter was thirteen, I took what might have
-				been my hardest challenge, which was to return to the US, to ensure she could pursue her education.
-				Since my return, I have worked for a small non-profit, degree-granting college supporting the
-				apprenticeship model of midwifery education. I have continuously sought to improve my skills and
-				capabilities, and have been able to rise to the position of Chief Operations Officer. During my
-				time there, I was able to look at the outcome we were seeking, and implement structural systems
-				changes that have allowed this school to increase student matriculation from less than 20 to over
-				160, while keeping expenses and staffing needs low. I realized we needed a new website and
-				database, so learned how to make these, and then made them for the College. I am overjoyed to
-				be able to step away from the College and move forward in my education to learn full-stack
-				coding and digital development - an area which has called to me since Thailand, when I was designing t-
-					shirts and brochures for my businesses. The adventure has just begun!</p>
+			<div class="container text-left">
+				<div class="row">
+					<p>About me:</p>
+				</div>
+				<div class="row">
+					<div class="col-sm-4">
+						<p>At eighteen, I set off towards Nepal with the intention of volunteering in a women’s shelter for a
+							few months before going to college. On the way there, I stopped in Thailand, went to an island
+							and took a scuba diving course, got caught in a typhoon, and missed my flight to Nepal. I never
+							went on to Nepal, never went back to college, and ended up living in Thailand for the next eighteen
+							years. During that time, I found I could pursue my interests for the sheer joy of learning and
+							adventure. </p>
+					</div>
+					<div class="col-sm-4">
+						<p>I realized that my hard work and perseverance could bring me success. I pushed through
+							the self-doubt that comes from following the unbeaten path, and managed to teach myself to
+							speak Thai, start several businesses, provide services to clients from all over the world, and learned
+							how to flourish in a different culture. When my daughter was thirteen, I took what might have
+							been my hardest challenge, which was to return to the US, to ensure she could pursue her education.
+							Since my return, I have worked for a small non-profit, degree-granting college supporting the
+							apprenticeship model of midwifery education. I have continuously sought to improve my skills and
+							capabilities,  </p>
+					</div>
+					<div class="col-sm-4">
+						<p>and have been able to rise to the position of Chief Operations Officer. During my
+							time there, I was able to look at the outcome we were seeking, and implement structural systems
+							changes that have allowed this school to increase student matriculation from less than 20 to over
+							160, while keeping expenses and staffing needs low. I realized we needed a new website and
+							database, so learned how to make these, and then made them for the College. I am overjoyed to
+							be able to step away from the College and move forward in my education to learn full-stack
+							coding and digital development - an area which has called to me since Thailand, when I was designing t-
+							shirts and brochures for my businesses. The adventure has just begun! </p>
+					</div>
+				</div>
+
 			</div>
 			<!--the div for Contact Form, should left when larger than sm, then center when sm-->
 			<div class="container text-center">
