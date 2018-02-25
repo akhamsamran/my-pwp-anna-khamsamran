840aa7af6fc32728e4fa47f87beeebfbbc5bfeac
akhamsamran <akamsamran1@cnm.edu>
2018-02-24 22:55:33 -0700

    working on visibility of text

--- public_html/index.php
+++ public_html/index.php
@@ -49,11 +49,13 @@
 
 				<div class="container pt-4 pb-5 text-right">
 					<div class="row">
-						<div class="col d-inline-block align-top pb-4">
-							<span><img src="images/annakhamsamran-blue-blue.svg" alt="Anna Khamsamran logo"> Data, Design, Development</span>
+						<div class="col-8 d-inline-block align-top pb-4">
+							<img src="images/annakhamsamran-blue-blue.svg" alt="Anna Khamsamran logo">
 						</div>
-					</div>
+						<div class="col-4 d-inline-block text-right">
+							<img src="images/data-design-dev.svg" alt="data, design, development">
 				</div>
+					</div>
 
 		</header>
 
@@ -69,19 +71,17 @@
 
 				</div>
 			</div>
-			<!--the div for Creative Philosophy, should align right-ish when larger than sm, then go to center when sm-->
-			<div class="container text-right">
+			<!--the div for Creative Philosophy, should align right-ish when larger-->
+			<div class="container mb-4 mt-5 text-center font-weight-bold lead">
 				<div class="row">
 					<div class="col-sm-8"></div>
 					<div class="col-sm-4">
-						<p>Philosophy:</p>
-						<p>My approach is to let the project speak to me and is based in empowerment, clarity and flexibility.
-						Clearing obstacles from success involves focusing on the end purpose, learning the systems used by
-							the team to reach them, and discovering how to streamline this for the most elegant and useful outcome</p>
+						<h3>Philosophy:</h3>
+						<p>Easy access to well organized information  improves business and customer experience. My goal is to build systems and tools that remove obstacles through elegant structure and sound design.</p>
 					</div>
-					<div class="col-sm-1"></div>
-				</div>
 			</div>
+			</div>
+
 			<!--the div for portfolio carousel, should align center in all sizes-->
 			<div class="container-fluid bg-light text-center">
 				<!--Carousel Wrapper-->
@@ -201,39 +201,19 @@
 			<!--the div for About Me-->
 			<div class="container text-left">
 				<div class="row">
-					<p>About me:</p>
+					<h3>About me:</h3>
 				</div>
 				<div class="row">
-					<div class="col-sm-4">
-						<p>At eighteen, I set off towards Nepal with the intention of volunteering in a women’s shelter for a
-							few months before going to college. On the way there, I stopped in Thailand, went to an island
-							and took a scuba diving course, got caught in a typhoon, and missed my flight to Nepal. I never
-							went on to Nepal, never went back to college, and ended up living in Thailand for the next eighteen
-							years. During that time, I found I could pursue my interests for the sheer joy of learning and
-							adventure. </p>
+					<div class="col-sm-3 m-1 p-3 bg-light">
+						<p>At eighteen, I set off towards Nepal with the intention of volunteering in a women’s shelter for a few months before going to college. On the way there, I stopped in Thailand, went to an island, got caught in a typhoon, and missed my flight onwards. I became a scuba diving instructor and ended up living in Thailand for the next eighteen years.</p>
 					</div>
-					<div class="col-sm-4">
-						<p>I realized that my hard work and perseverance could bring me success. I pushed through
-							the self-doubt that comes from following the unbeaten path, and managed to teach myself to
-							speak Thai, start several businesses, provide services to clients from all over the world, and learned
-							how to flourish in a different culture. When my daughter was thirteen, I took what might have
-							been my hardest challenge, which was to return to the US, to ensure she could pursue her education.
-							Since my return, I have worked for a small non-profit, degree-granting college supporting the
-							apprenticeship model of midwifery education. I have continuously sought to improve my skills and
-							capabilities,  </p>
+					<div class="col-sm-3 m-1 p-3 bg-light">
+						<p>Upon my return to the US, I worked for a small non-profit, degree-granting college supporting the apprenticeship model of midwifery education. I have continuously sought to improve my skills and capabilities.  I designed set up systems and tools that greatly improved the business, and was able to rise to the position of Chief Operations Officer </p>
 					</div>
-					<div class="col-sm-4">
-						<p>and have been able to rise to the position of Chief Operations Officer. During my
-							time there, I was able to look at the outcome we were seeking, and implement structural systems
-							changes that have allowed this school to increase student matriculation from less than 20 to over
-							160, while keeping expenses and staffing needs low. I realized we needed a new website and
-							database, so learned how to make these, and then made them for the College. I am overjoyed to
-							be able to step away from the College and move forward in my education to learn full-stack
-							coding and digital development - an area which has called to me since Thailand, when I was designing t-
-							shirts and brochures for my businesses. The adventure has just begun! </p>
+					<div class="col-sm-3 m-1 p-3 bg-light">
+						<p>Working in digital development provides me with the exciting opportunity to be creative and pursue my passion.  Organizing and designing databases. It is enormously satisfying to set up systems that work elegantly and allow streamlined function. The adventure has just begun! </p>
 					</div>
 				</div>
-
 			</div>
 			<!--the div for Contact Form, should left when larger than sm, then center when sm
 			<div class="container text-center">
